6077f0dac9d7bda8080aa8afd4f5477ea5abfc56
saipranathi <saipranathikaitha@gmail.com>
2024-11-29 12:38:18 +0530

    second

--- src/Demo1/prg3.java
+++ src/Demo1/prg3.java
@@ -1,10 +1,10 @@
 package Demo1;
 
-public class Prg2 {
+public class prg3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hii");
+		System.out.println("welcome");
 	}
 
 }
