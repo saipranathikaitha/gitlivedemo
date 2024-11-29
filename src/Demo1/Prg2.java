4a281cd5fe011bd72efce3a79e89c854a5ac9120
saipranathi <saipranathikaitha@gmail.com>
2024-11-29 12:23:56 +0530

    first commit

---
+++ src/Demo1/Prg2.java
@@ -0,0 +1,10 @@
+package Demo1;
+
+public class Prg2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi");
+	}
+
+}
---
+++ src/Demo1/Program1.java
@@ -0,0 +1,13 @@
+package Demo1;
+
+public class Program1 {
+
+	public static void main(String[] args) {
+	
+		System.out.println("hello");
+		}
+	}
+	
+
+	
+
